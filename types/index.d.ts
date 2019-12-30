7c6a1b6b314940ff16c4ee950bbf30c3d6e7a85c
大板栗 <576839360@qq.com>
2019-12-30 14:02:30 +0800

    📝 Update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,13 @@
 declare namespace PIXI {
     export interface Container {
+        putCenter(this: any, global?: boolean): void
+        putCenterX(this: any, global?: boolean): void
+        putCenterY(this: any, global?: boolean): void
+        putLeft(this: any, global?: boolean): void
+        putTop(this: any, global?: boolean): void
+        putRight(this: any, global?: boolean): void
+        putBottom(this: any, global?: boolean): void
+
         putCenter(this: any, offsetX?: number, offsetY?: number, global?: boolean): void
         putCenterX(this: any, offsetX?: number, global?: boolean): void
         putCenterY(this: any, offsetY?: number, global?: boolean): void
