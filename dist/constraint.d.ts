be0d72e9ad2c8823681812162ddb32c482550912
大板栗 <576839360@qq.com>
2020-06-08 17:22:46 +0800

    📝 Update types

--- dist/constraint.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function putCenter(target: any, offsetX?: number, offsetY?: number, global?: boolean): void;
-export declare function putCenterX(target: any, offsetX?: number, global?: boolean): void;
-export declare function putCenterY(target: any, offsetY?: number, global?: boolean): void;
-export declare function putLeft(target: any, offsetX?: number, global?: boolean): void;
-export declare function putTop(target: any, offsetY?: number, global?: boolean): void;
-export declare function putRight(target: any, offsetX?: number, global?: boolean): void;
-export declare function putBottom(target: any, offsetY?: number, global?: boolean): void;
-//# sourceMappingURL=constraint.d.ts.map
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,27 @@
-import * as _constraint from './constraint';
-import Event from 'eventemitter3';
-export declare const constraints: typeof _constraint;
-export { fix, fixX, fixY } from './common';
-export declare function deprecatedConstraint({ Container }: any): void;
-export default function constraint(event: Event): void;
-export declare function createConstraint(width: number, height: number): typeof constraint;
-//# sourceMappingURL=index.d.ts.map
+declare namespace PIXI {
+    export interface Container {
+        putCenter(offsetX?: number, offsetY?: number, global?: boolean): void
+        putCenterX(offsetX?: number, global?: boolean): void
+        putCenterY(offsetY?: number, global?: boolean): void
+        putLeft(offsetX?: number, global?: boolean): void
+        putTop(offsetY?: number, global?: boolean): void
+        putRight(offsetX?: number, global?: boolean): void
+        putBottom(offsetY?: number, global?: boolean): void
+
+        putCenter(global?: boolean): void
+        putCenterX(global?: boolean): void
+        putCenterY(global?: boolean): void
+        putLeft(global?: boolean): void
+        putTop(global?: boolean): void
+        putRight(global?: boolean): void
+        putBottom(global?: boolean): void
+    }
+}
+
+declare module '@amoy/constraint' {
+    export function fix(target: any): void
+    export function fixX(target: any): void
+    export function fixY(target: any): void
+    export function createConstraint(width: number, height: number): () => void
+    export default function constraint(PIXI: any): any
+}
